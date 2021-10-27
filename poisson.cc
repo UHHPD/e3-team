e1347fc63737c472971485f9b94f24e2828cad58
TimChris <92857745+TimChris@users.noreply.github.com>
2021-10-27 11:03:40 +0000

    a), part of b)

--- poisson.cc
+++ poisson.cc
@@ -10,11 +10,19 @@ double poisson(double mu, int k) {
 int main() {
     using namespace std;
 
+    vector<int> zaehler(11);
 
     ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i] += 1;
     }
     fin.close();
+    ofstream fout("hist.txt");
+    for(unsigned int k = 0 ; k < zaehler.size() ; ++k) {
+      std::cout << k << ":" << zaehler[k] << std::endl;
+      fout << k << " " << zaehler[k] << std::endl;
+    }
+    fout.close();
 }
