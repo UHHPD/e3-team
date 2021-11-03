41ed65df27611d1bd79864a8784a49f16a9811f8
katneum <92448655+katneum@users.noreply.github.com>
2021-11-03 15:33:27 +0000

    Aufgabe 2

--- likelihood.cc
+++ likelihood.cc
@@ -16,6 +16,17 @@ long double prob(std::vector<int> daten, long double mu) {
     return L;
 
 }
+long double probk(std::vector<int> daten) {
+  long double A = 1;
+  for(int k:daten) {
+    A*=poisson(k,k);
+  }
+  //std::cout << L << std::endl;
+    return A;
+}
+long double Lambda(std::vector<int> daten,double mu){
+  return prob(daten,mu)/probk(daten);
+}
 int main() {
     using namespace std;
     
@@ -41,8 +52,17 @@ int main() {
     }
     fout2.close();
     ofstream fout3("deltanll.txt");
-    for(double mu=0;mu<=6;mu+=0.01){
-      fout3 << mu << " " << (-2)*log(prob(daten,mu))-(-2)*log(prob(daten,3.11538)) << std::endl;
+    for(double mu=0;mu<=6;mu+=0.001){
+      fout3 << mu << " " << (-2)*log(prob(daten,mu))-(-2)*log(prob(daten,3.115)) << std::endl;
     }
     fout3.close();
+  ofstream fout4("test2.txt");
+  fout4 << -2*log(Lambda(daten,3.11538))<< std::endl;
+  std::cout<<-2*log(Lambda(daten,3.11538))<< std::endl;
+  fout4.close();
+  ofstream fout5("reldiv.txt");
+  fout5<<(-2*log(Lambda(daten,3.11538))-233)/(sqrt(2*233))<<std::endl;
+  std:cout<<(-2*log(Lambda(daten,3.11538))-233)/(sqrt(2*233))<<std::endl;
+  fout4.close();
+
 }
